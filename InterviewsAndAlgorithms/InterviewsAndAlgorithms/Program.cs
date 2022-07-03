d4fa67a77115895a8f3cea70c651d01a5b094183
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-03 08:05:54 +0200

    Pierwszy Commit

---
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 2; i < 100; i++)
+{
+    if (LiczbyPierwsze(i))
+    {
+        Console.WriteLine("Liczba pierwsza: " + i + " jest pierwsza");
+    }
+}
+
+bool LiczbyPierwsze(int a)
+{
+    if (a <= 2)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= Math.Sqrt(a); i++)
+    {
+        if (a % i ==0)
+        {
+            return false;
+        }
+
+    }
+        return true;
+}
