9726ff6539bacc0f4562341174eb3989603a9eac
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-06 13:04:12 +0200

    Strong Number 1

--- InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
@@ -1,30 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int firstNumber = 0;
-int secondNumber = 1;
 
-for (int i = 2; i <= 10; i++)
-{
-    int result = firstNumber + secondNumber;
-    firstNumber = secondNumber;
-    secondNumber = result;
-    Console.WriteLine(result);
+int number = 1;
 
-}
-Console.WriteLine("=====================");
-for (int i = 1; i < 15; i++)
+int n = 6;
+for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine($" {Fib(i)}");
+    number = number * i;
 }
 
-static int Fib(int x)
-{
-    if (x < 2)
-    {
-        return x;
-    }
-    else
-    {
-        return Fib(x - 1) + Fib(x - 2);
-    }
-}
+Console.WriteLine($"Factorial of {n} is: {number}" );
