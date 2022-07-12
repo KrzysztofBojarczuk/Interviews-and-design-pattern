c3451800f595f68bbb1b987a1d60ad71eb3959dc
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-07-12 15:34:18 +0200

    Facade

--- InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
@@ -1,21 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using InterviewsAndAlgorithms.Facade;
 using InterviewsAndAlgorithms.Singleton;
 
 int[] tablica = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//Singleton Pattern 
+//We want to create one object and use it everywhere
+//Facade Pattern
+//Tworzymy jedną klasę fasadę która odpowiada za wszystkie zadania.
+//We create one class of facade that is responsible for all tasks.
 
 Singleton object1 = Singleton.GetInstance();
 Singleton object2 = Singleton.GetInstance();
 
 
-if (object1 == object2)
-{
-    Console.WriteLine("There objects are the same");
-}
-else
-{
-    Console.WriteLine("There objects are different");
-}
+NetworkFacade networkFacade = new NetworkFacade("1.1.1.1.","UDP");
 
-int val = 0;
+networkFacade.BuildNetworkLayer();
+networkFacade.SendPAcketOverNetwork();
