b4682783380ec3545c57576b5b16df4901808203
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2022-07-15 12:29:25 +0200

    Update DataType.cs

--- InterviewsAndAlgorithms/InterviewsAndAlgorithms/DataType.cs
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/DataType.cs
@@ -21,6 +21,9 @@ namespace InterviewsAndAlgorithms
         //Stored on heap                                        stack
 
         //Parametrs is definition of a varaible in a method of declarations. Arguemnt the value passed to the method paramentr.
+        //Generic Class generic means not specific to a particular data type
+
+       //Generic classes are defined using a type parameter in an angle brackets after the class name. The following defines a generic class. 
         
     }
 }
