9d9327f68a5ec129794530eb1f2e25a94c59e29b
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-08-08 10:39:05 +0200

    Task

--- InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
@@ -8,8 +8,13 @@ DelegateClass delegateClass = new DelegateClass();
 
 
 Func<int, int,int> funcDelegate = delegateClass.Calc;
+
 Console.WriteLine(funcDelegate(5,5));
+
 Action actionDelgate = delegateClass.Show;
+
 actionDelgate();
+
 Predicate<int> predicateDelgate = delegateClass.GraterThenTen;
+
 Console.WriteLine(predicateDelgate(15));
