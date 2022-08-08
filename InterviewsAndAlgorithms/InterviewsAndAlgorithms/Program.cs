9f368bf9623726b44311e8aa17b1188d16cec874
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-08-08 10:58:35 +0200

    multitthreading

--- InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/Program.cs
@@ -1,20 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using InterviewsAndAlgorithms.Delegates;
+using InterviewsAndAlgorithms.ThreadNameSpace;
 
 
 
 
-DelegateClass delegateClass = new DelegateClass();
+
+// thread = an execution path of a program
+//          We can use multiple threads to perform,
+//          different tasks of our program at the same time.
+//          Current thread running is "main" thread
+//          using System.Threading;
+
+// wątek = ścieżka wykonania programu
+// Możemy użyć wielu wątków do wykonania,
+// różne zadania naszego programu w tym samym czasie.
+// Bieżący wątek to „główny” wątek
+// za pomocą System.Threading;
+
+Thread mainThread = Thread.CurrentThread;
+
+mainThread.Name = "Main Thread";
 
 
-Func<int, int,int> funcDelegate = delegateClass.Calc;
+ThreadClass threadClass = new ThreadClass();
 
-Console.WriteLine(funcDelegate(5,5));
 
-Action actionDelgate = delegateClass.Show;
+Thread thread1 = new Thread(() => threadClass.CountDown("Timer #1"));
 
-actionDelgate();
+Thread thread2 = new Thread(() => threadClass.CountUp("Timer #2"));
+thread1.Start();
 
-Predicate<int> predicateDelgate = delegateClass.GraterThenTen;
+thread2.Start();
 
-Console.WriteLine(predicateDelgate(15));
+Console.WriteLine(mainThread.Name + " is complete!");
+
+
+Console.ReadKey();
---
+++ InterviewsAndAlgorithms/InterviewsAndAlgorithms/ThreadNameSpace/ThreadClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewsAndAlgorithms.ThreadNameSpace
+{
+    public class ThreadClass
+    {
+        public  void CountDown(String name)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine("Timer #1 : " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #1 is complete!");
+        }
+        public  void CountUp(String name)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(name + " : " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #2 is complete!");
+        }
+    }
+}
